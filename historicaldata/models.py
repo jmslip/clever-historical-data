e0d14acfa53df0c25694d5bdad7e10344b4451cf
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-03-19 08:43:22 -0300

    Adicionado Models, admin e conexão com o banco
    Adicionado Modelos Ativo e HistoricalData, configurado Admin para os
    dois modelos e configurado conexão com banco de dados Postgres.

--- historicaldata/models.py
+++ historicaldata/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Ativo(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    simbolo = models.SlugField('Símbolo', max_length=100)
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Ativo'
+        verbose_name_plural = 'Ativos'
+        ordering = ['nome']
+
+
+class HistoricalData(models.Model):
+    data_historico = models.DateTimeField('Data')
+    ultimo = models.FloatField('Último')
+    variacao = models.FloatField('Var%')
+    ativo = models.ForeignKey(Ativo, verbose_name='Ativo', on_delete=models.CASCADE)
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Dado Histórico'
+        verbose_name_plural = 'Dados Históricos'
+        ordering = ['data_historico', 'ativo']
