d0041971ac330408c0b2ecfa3d5c4aedc1e88aa8
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-03-25 12:15:37 -0300

    Implementado recurso de Histórico
    Implementado recurso de histórico para trazer dados passados
    do banco de dados.

---
+++ core/serializable.py
@@ -0,0 +1,47 @@
+import decimal
+from json import dumps, loads
+from json.encoder import JSONEncoder
+from typing import Any
+
+from core.models import Ativo as AtivoModel, HistoricalData as HistoricoModel
+
+class AtivoSerializable:
+
+    def __init__(self, ativoModel: AtivoModel) -> None:
+        self.data_criacao = ativoModel.created
+        self.simbolo = ativoModel.simbolo
+        self.nome = ativoModel.nome
+        self.pais = ativoModel.pais
+
+    
+    def to_json(self):
+        dicionario = {
+            "data_criacao": self.data_criacao.isoformat(),
+            "simbolo": self.simbolo,
+            "nome": self.nome,
+            "pais": self.pais
+        }
+
+        return loads(dumps(dicionario))
+
+
+class HistoricoSerializable:
+
+    def __init__(self, historicoModel: HistoricoModel) -> None:
+        self.data_criacao = historicoModel.created
+        self.data_historico = historicoModel.data_historico
+        self.ultimo = historicoModel.ultimo
+        self.variacao = historicoModel.variacao
+        self.ativo = AtivoSerializable(historicoModel.ativo)
+
+    
+    def to_json(self):
+        dicionario = {
+            "data_criacao": self.data_criacao.isoformat(),
+            "data_historico": self.data_historico.isoformat(),
+            "ultimo": str(self.ultimo),
+            "variacao": str(self.variacao),
+            "ativo": self.ativo.to_json()
+        }
+
+        return loads(dumps(dicionario))
