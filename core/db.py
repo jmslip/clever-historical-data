b122730fda282cd44a0ebe7f1672d5302758af84
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-03-25 06:46:46 -0300

    Correção de PR

--- core/db.py
+++ core/db.py
@@ -5,4 +5,4 @@ from instance.config import DB_ADDR, DB_NAME, DB_PASS, DB_USER
 class DB:
 
     def configure(self):
-        return PooledPostgresqlDatabase(DB_NAME, max_connections=8, stale_timeout=50, user=DB_USER, password=DB_PASS, host=DB_ADDR)
+        return PooledPostgresqlDatabase(DB_NAME, max_connections=8, stale_timeout=300, user=DB_USER, password=DB_PASS, host=DB_ADDR)
