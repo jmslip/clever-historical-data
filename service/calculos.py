c1bbc3ece649f21b75789aa05d56653da83f60e9
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-03-31 18:50:22 -0300

    Implementando calculos
    Adicionando resouce calculos ao projeto

--- service/calculos.py
+++ service/calculos.py
@@ -1,16 +1,23 @@
 from datetime import timedelta, datetime
+from pandas import DataFrame
 
 from service.historico import HistoricoService
+from utils.clever_generics import CleverGenerics
 
 class Calculos:
     def __init__(self) -> None:
         self.historicoService = HistoricoService()
+        self.clever_generics = CleverGenerics()
     
     def covariancia(self, ativos):
         historico = 0
-        to_date = (datetime.today() + timedelta(days=-365*2))
-        from_date = datetime.today
+        from_date = self.clever_generics.data_formato_br((datetime.today() + timedelta(days=-365*2)))
+        to_date = self.clever_generics.data_formato_br(datetime.today())
 
         for ativo in ativos:
-            historico = self.historicoService.passado(ativo, to_date=to_date, from_date=from_date)
+            historico = self.historicoService.passado(ativo, to_date=to_date, from_date=from_date, model_to_json=True)
+            
+        
+            df = DataFrame(historico)
+            print(df)
 
