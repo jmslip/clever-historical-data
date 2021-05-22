9c4f3102c0fdd1dca2f96f98f92c30682e1aa2f3
Jorge M Souza Junior <jms.slip@gmail.com>
2021-05-22 15:27:36 -0300

    Implementado cálculo de RD

--- resources/calculos.py
+++ resources/calculos.py
@@ -1,13 +1,51 @@
 from flask_restplus import Resource
+import pandas
 
 from core.server import server
+from service.ativos import Ativos
 from service.calculos import Calculos as CalculosService
+from utils.clever_generics import CleverGenerics
 
 api = server.api
 calculosService = CalculosService()
+clever_generics = CleverGenerics()
+ativosService = Ativos()
+
+parametros_obrigatorios = ['ativos', 'perfil']
+
+conservador = 'B5P211'
+agressivo = '0P0001HL9K'
+moderado = [conservador, agressivo]
 
 @api.route('/calculos')
 class Calculos(Resource):
     @staticmethod
     def get():
-        calculosService.covariancia(['XINA11', 'BOVA11'])
+        request = api.payload
+
+        err = clever_generics.valida_parametros_obrigatorios(request=request, parametros=parametros_obrigatorios)
+
+        if err is not None:
+            return err
+        
+        perfil = request['perfil']
+        ativosParaCalculo = []
+        if perfil == '1':
+            pesquisa = ativosService.pesquisa(ativo=conservador)
+            ativosParaCalculo.append(pesquisa.symbol)
+        elif perfil == '3':
+            pesquisa = ativosService.pesquisa(ativo=agressivo)
+            ativosParaCalculo.append(pesquisa.symbol)
+        else:
+            for ativo in moderado:
+                pesquisa = ativosService.pesquisa(ativo=ativo)
+                ativosParaCalculo.append(pesquisa.symbol)
+
+
+        ativos = request['ativos']
+        for ativo in ativos:
+            for valor in ativo:
+                ativosParaCalculo.append(valor)
+
+        return calculosService.rd(ativos=ativosParaCalculo)
+        
--- service/calculos.py
+++ service/calculos.py
@@ -1,5 +1,6 @@
 from datetime import timedelta, datetime
-from pandas import DataFrame
+import pandas as pd
+import numpy as np
 
 from service.historico import HistoricoService
 from utils.clever_generics import CleverGenerics
@@ -8,16 +9,44 @@ class Calculos:
     def __init__(self) -> None:
         self.historicoService = HistoricoService()
         self.clever_generics = CleverGenerics()
-    
-    def covariancia(self, ativos):
-        historico = 0
+
+    def rd(self, ativos):
         from_date = self.clever_generics.data_formato_br((datetime.today() + timedelta(days=-365*2)))
         to_date = self.clever_generics.data_formato_br(datetime.today())
-
+        historico = dict()
         for ativo in ativos:
-            historico = self.historicoService.passado(ativo, to_date=to_date, from_date=from_date, model_to_json=True)
-            
+            historico[ativo] = self.historicoService.passado(ativo, to_date=to_date, from_date=from_date, model_to_json=True)
         
-            df = DataFrame(historico)
-            print(df)
+        desvioPadrao = self.calculaDesvioPadrao(historico=historico)
+        
+        desvioNormalizado = desvioPadrao.apply(self.normalize)
+        somaDesvioNormalizado = self.getSomaDesvio(desvioNormalizado)
 
+        rd = dict()
+        for chave, item in desvioNormalizado.iteritems():
+            rd[chave] = self.calculaRd(item[0], somaDesvioNormalizado)
+        
+        return rd
+
+    def calculaDesvioPadrao(self, historico: dict):
+        historicoVariacao = dict()
+        for ativo in historico:
+            variacao = []
+            for item in historico[ativo]['historico']:
+                variacao.append(float(item['variacao']))
+            historicoVariacao[ativo] = variacao
+
+        df = pd.DataFrame(historicoVariacao)
+        dp = df.std()
+        return  pd.DataFrame(dp).transpose()
+
+    def normalize(self, x):
+        return 1/x
+
+    def getSomaDesvio(self, desvio):
+        somaDesvioSeries = desvio.sum(axis=1)
+        somaDesvio = somaDesvioSeries.values
+        return somaDesvio[0]
+
+    def calculaRd(self, desvioNormalizado, somaDesvioNormalizado):
+        return (desvioNormalizado / somaDesvioNormalizado)
