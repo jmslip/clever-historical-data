7c9ff372805dec3e24fb63f9b64d2ec8ad0a1f25
Jorge M Souza Junior <jms.slip@gmail.com>
2021-05-23 01:08:54 -0300

    Log

--- service/calculos.py
+++ service/calculos.py
@@ -19,6 +19,7 @@ class Calculos:
             historico_ativo = self.historicoService.passado(ativo, to_date=to_date, from_date=from_date, model_to_json=True)
             if ativo == 'BOVA11':
                 print(historico_ativo)
+                print(len(historico_ativo))
             if historico_ativo is not None or len(historico_ativo) > 0:
                 historico[ativo] = historico_ativo
         
